8f2941c46dbf66d9c67bbac5847f4ee012fbd167
Your Name <you@example.com>
2021-12-06 14:01:37 +0700

    upload source code

---
+++ PhpTravel_POM_DiemHL1/PHP_Travel_POM/RestSharpAPI/CreateUser.cs
@@ -0,0 +1,9 @@
+﻿namespace PHP_Travel_POM.RestSharpAPI
+{
+    public class CreateUser
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string job { get; set; }
+    }
+}
