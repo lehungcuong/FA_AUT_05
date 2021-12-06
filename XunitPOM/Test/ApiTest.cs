b8400bd2735c06222fcae98a5442f5062e2e0e70
Doan Hieu <baolam0000@gmail.com>
2021-12-06 23:30:14 +0700

    Optimize api test case code

--- XunitPOM/Test/ApiTest.cs
+++ XunitPOM/Test/ApiTest.cs
@@ -18,6 +18,7 @@ namespace XunitPOM.Test
             apiHelper = new ApiHelper(ConfigHelper.GetValue("HostPath"));
         }
 
+        [Trait("Category", "Example 1")]
         [Fact(DisplayName = "Api Test")]
         public void ApiRawTest()
         {
@@ -30,25 +31,12 @@ namespace XunitPOM.Test
             // Create new user by API and add to Jsondata
             var CreateNewUser = apiHelper.SendPostRequest<Dictionary<string, string>>(ConfigHelper.GetValue("PathPostUser"), parameter);
             BrowserFactory.AssertValueBool(CreateNewUser.ContainsKey("userID"), AssertType.True, "Can't find userID in data");
-            foreach ( var element in CreateNewUser)
-            {
-                if (element.Key == "userID")
-                {
-                    JsonHelper.SetValueByKeyJson(element.Value, element.Key);
-                }  
-            }
+            JsonHelper.SetValueByKeyJson(CreateNewUser["userID"], "userID");
 
             // Create new Token by API and add to Jsondata
             var CreateToken = apiHelper.SendPostRequest<Dictionary<string, string>>(ConfigHelper.GetValue("PathToken"), parameter);
             BrowserFactory.AssertValueBool(CreateToken.ContainsKey("token"), AssertType.True, "Can't find token in data");
-            foreach (var element in CreateToken)
-            {
-                if (element.Key == "token")
-                {
-                    JsonHelper.SetValueByKeyJson(element.Value, element.Key);
-                }
-                    
-            }
+            JsonHelper.SetValueByKeyJson(CreateToken["token"], "token");
 
             // Save token and uuid into JsonData
             JsonHelper.SaveJson();
@@ -56,17 +44,8 @@ namespace XunitPOM.Test
             // Get user information by API and compare with value in TestData
             var GetUserInformation = apiHelper.SendGetRequest<Dictionary<string, string>>(ConfigHelper.GetValue("PathGetUser"), JsonHelper.GetValueByKeySavedData("userID"));
             BrowserFactory.AssertValueBool(GetUserInformation.ContainsKey("userId") && GetUserInformation.ContainsKey("username"), AssertType.True, "Can't find userID or userName");
-            foreach (var element in GetUserInformation)
-            {
-                if (element.Key == "userId")
-                {
-                    BrowserFactory.AssertValueEqual(element.Value, JsonHelper.GetValueByKeySavedData("userID"), "Wrong userid from API !");
-                }
-                else if (element.Key == "username")
-                {
-                    BrowserFactory.AssertValueEqual(element.Value, JsonHelper.GetValueByKeyRawData("userName"), "Wrong username from API !");
-                }
-            }
+            BrowserFactory.AssertValueEqual(GetUserInformation["userId"], JsonHelper.GetValueByKeySavedData("userID"), "Wrong userid from API !");
+            BrowserFactory.AssertValueEqual(GetUserInformation["username"], JsonHelper.GetValueByKeyRawData("userName"), "Wrong username from API !");
 
             // Delete user by API and compare return content
             BrowserFactory.AssertValueEmpty(apiHelper.SendDeleteRequest(ConfigHelper.GetValue("PathGetUser"), JsonHelper.GetValueByKeySavedData("userID")), "Return data not null !!");
