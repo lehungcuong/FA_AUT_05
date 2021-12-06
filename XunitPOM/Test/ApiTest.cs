ae4fc490498d1c3e55bd9622b24714686a6b15c3
Doan Hieu <baolam0000@gmail.com>
2021-12-06 23:50:42 +0700

    Delete bin and obj

--- XunitPOM/Test/ApiTest.cs
+++ XunitPOM/Test/ApiTest.cs
@@ -18,7 +18,7 @@ namespace XunitPOM.Test
             apiHelper = new ApiHelper(ConfigHelper.GetValue("HostPath"));
         }
 
-        [Trait("Category", "Example")]
+        [Trait("Category", "Run")]
         [Fact(DisplayName = "Api Test")]
         public void ApiRawTest()
         {
