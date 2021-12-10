a2335276a5208b67746a77ac24a2278baa376640
Your Name <you@example.com>
2021-12-10 07:58:37 +0700

    upload source code

--- PhpTravel_POM_DiemHL1/PHP_Travel_POM/RestSharpAPI/CreateUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PHP_Travel_POM.RestSharpAPI
-{
-    public class CreateUser
-    {
-        public string id { get; set; }
-        public string name { get; set; }
-        public string job { get; set; }
-    }
-}
--- PhpTravel_POM_DiemHL1/PHP_Travel_POM/Utilities/JsonHelper.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace PHP_Travel_POM.Utilities
-{
-    public class JsonHelper
-    {
-        private static Dictionary<string, string> _Data;
-    }
-}
