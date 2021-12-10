cc981d7569da0c2dc89028620315dfdf2de80005
Nguyen Thi Son Ca <1754052006ca@ou.edu.vn>
2021-12-10 07:59:03 +0700

    delete file

--- DemoPOM/DemoPOM/RestSharpAPI/CreateUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DemoPOM.RestSharpAPI
-{
-    public class CreateUser
-    {
-        public string id { get; set; }
-        public string name { get; set; }
-        public string job { get; set; }
-    }
-}
